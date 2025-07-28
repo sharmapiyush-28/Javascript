a818aa5db035b7c9e25e0a9abe8809eeea9e24df
Piyush Sharma <sharmapiyush5783@gmail.com>
2025-07-28 16:15:10 +0530

    add new program

---
+++ 01_basics.js/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "sharmapiyush5784@gmail.com"
+var accountPassword = "12345"
+accountCity = "Chandigarh"
+let accountState;
+
+//accountId = 2
+accountEmail ="sharmapiyush47839@gmail.com"
+accountPassword ="182903"
+accountCity = "Mohali"
+console.table([accountEmail, accountCity, accountPassword, accountState])
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
