df0a37ed3a21acb693e8e45eca42cb26ecefca20
Piyush Sharma <sharmapiyush5783@gmail.com>
2025-07-28 16:15:59 +0530

    add new program

---
+++ 01_basics.js/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "Piyush"
+let age = 19
+let isLoggedIn = true
+let state;
+/*
+number => 2 to power 53
+bigint
+string =""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => for unique
+
+object
+*/
+
+console.log(typeof "hitesh")
