255275c3a064ac0970f5ec9de2dae6e6c1776114
Piyush Sharma <sharmapiyush5783@gmail.com>
2025-07-28 16:36:57 +0530

    add new program

---
+++ 01_basics.js/03_conversionoperations.js
@@ -0,0 +1,32 @@
+/*let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1
+// undefined -> NaN
+// Null -> 0
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanisLoggedIn);
+
+// 1 -> true 
+// 0 -> false
+// "" -> false
+// "Piyush" -> true
+
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
