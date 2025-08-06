8562b56ffe2731b343f633c0bab0d0a76a30525e
Piyush Sharma <sharmapiyush5783@gmail.com>
2025-08-06 15:47:24 +0530

    new file added

--- 06_strings.js
+++ 06_strings.js
@@ -4,7 +4,7 @@ const repoCount = 10
 console.log(name + repoCount + "Value");
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('Piyushsharma')
+const gameName = new String('Piyushsharma-ps')
 console.log(gameName);
 console.log(gameName[0]);
 console.log(gameName.__proto__);   // this gave the object
@@ -21,3 +21,15 @@ console.log(newString);
 
 const anotherString = gameName.slice(-12, 7)
 console.log(anotherString);
+
+const newStringOne = "  piyush  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://piyush.com/piyush%20sharma"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('savage'))
+
+console.log(gameName.split('-'))
