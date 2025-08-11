becee2110f1f1989107c241b4a6099bf23e0a4de
Piyush Sharma <sharmapiyush5783@gmail.com>
2025-08-11 23:57:13 +0530

    add new file

--- 02_basics.js/05_functions.js
+++ 02_basics.js/05_functions.js
@@ -34,5 +34,37 @@ function loginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Piyush"))
-console.log(loginUserMessage())
+//console.log(loginUserMessage("Piyush"))
+//console.log(loginUserMessage())
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 600))
+
+const user = {
+    username: "Piyush",
+    price: "199"
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 800, 500, 1000]));
+
