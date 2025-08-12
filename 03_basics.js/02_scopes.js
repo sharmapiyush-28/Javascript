d63772640e05273ca6766f9ae255ce7c0be67b88
Piyush Sharma <sharmapiyush5783@gmail.com>
2025-08-13 00:55:38 +0530

    add new file

--- 03_basics.js/02_scopes.js
+++ 03_basics.js/02_scopes.js
@@ -1,7 +1,49 @@
-let a = 10
-const b = 20
-var c = 30
+//var c = 300 
+let a = 300
+if (true){
+    let a = 10
+    const b = 20
+    //console.log("INNER: ", a);
+}
 
-console.log(a);
-console.log(b);
-console.log(c);
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one(){
+    const username = "Piyush"
+    function two() {
+        const website = "Youtube"
+        console.log(username);
+    }
+    //console.log(website)
+    two()
+}
+ 
+//one()
+
+if(true){
+    const username = "Piyush"
+    if(username === "Piyush"){
+        const website = "Youtube"
+        console.log(username + website);
+    }
+    //console.log(website);
+}
+
+//console.log(username);
+
+// +++++++++++++ interesting ++++++++++++++
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+
+//addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+
